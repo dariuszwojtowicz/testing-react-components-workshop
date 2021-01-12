84c85233da95c260de517acfb6b60f167879e279
Dariusz Wojtowicz <dariusz.wojtowicz@allegro.pl>
2021-01-12 08:52:51 +0100

    Shared Mocked Module

---
+++ tests/mocking-shared-mock-module.spec.ts
@@ -0,0 +1,25 @@
+import random from '../src/utils/random';
+import {greet, greet2} from '../src/greetings';
+
+// TODO mock random and config module
+
+describe('greetings with shared mocked module', () => {
+
+  test('should return correct greeting for given name (lang)', () => {
+    // when
+    const greeting = greet('Joe');
+
+    // then
+    expect(greeting).toEqual('Cześć Joe!');
+  });
+
+  test('should return correct greeting for given name', () => {
+    // when
+    const greeting = greet2('Joe');
+
+    // then
+    expect(greeting).toEqual('Hello Joe!');
+    expect(random.getRandomListElement).toBeCalledTimes(1);
+    expect(random.getRandomListElement).toBeCalledWith('Hi', 'Hello', 'Whats up');
+  });
+});
