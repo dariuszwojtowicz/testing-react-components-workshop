7ed8f74537ba72a2195591ca3d2406c1c29c02ad
Dariusz Wojtowicz <dariusz.wojtowicz@allegro.pl>
2021-03-19 07:37:31 +0100

    Mocking Functions Solution

--- tests/mocking-functions.spec.ts
+++ tests/mocking-functions.spec.ts
@@ -3,6 +3,8 @@ import {greet2} from '../src/greetings';
 
 test('should return correct greeting for given name', () => {
   // given
+  const originalImplementation = random.getRandomListElement;
+  random.getRandomListElement = jest.fn(() => 'Hi');
 
   // when
   const greeting = greet2('Joe');
@@ -11,4 +13,20 @@ test('should return correct greeting for given name', () => {
   expect(greeting).toEqual('Hi Joe!');
   expect(random.getRandomListElement).toBeCalledTimes(1);
   expect(random.getRandomListElement).toBeCalledWith('Hi', 'Hello', 'Whats up');
+  random.getRandomListElement = originalImplementation;
+});
+
+test('should return correct greeting for given name with spyOn', () => {
+  // given
+  const mocked = jest.spyOn(random, 'getRandomListElement');
+  mocked.mockImplementation(() => 'Hello');
+
+  // when
+  const greeting = greet2('Joe');
+
+  // then
+  expect(greeting).toEqual('Hello Joe!');
+  expect(random.getRandomListElement).toBeCalledTimes(1);
+  expect(random.getRandomListElement).toBeCalledWith('Hi', 'Hello', 'Whats up');
+  mocked.mockRestore();
 });
