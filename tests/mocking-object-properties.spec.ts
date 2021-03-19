f836696f5684808feb08db33c84b831b0c11fc17
Dariusz Wojtowicz <dariusz.wojtowicz@allegro.pl>
2021-03-19 07:17:21 +0100

    Mocking Object Properties Solution

--- tests/mocking-object-properties.spec.ts
+++ tests/mocking-object-properties.spec.ts
@@ -1,11 +1,15 @@
 import {greet} from "../src/greetings";
+import config from "../src/utils/config";
 
 test('should return correct greeting for given name', () => {
   // given
+  const originalLang = config.lang;
+  config.lang = 'pl';
 
   // when
   const greeting = greet('Joe');
 
   // then
   expect(greeting).toEqual('Cześć Joe!');
+  config.lang = originalLang;
 });
