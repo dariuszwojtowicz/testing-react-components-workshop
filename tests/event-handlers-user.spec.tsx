1309fc0b7735fc0c20c155da6b2e6f739cde6da8
Dariusz Wojtowicz <dariusz.wojtowicz@allegro.pl>
2021-03-23 10:43:30 +0100

    Event Handlers Solution

--- tests/event-handlers-user.spec.tsx
+++ tests/event-handlers-user.spec.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import {fireEvent, render} from '@testing-library/react';
+import user from '@testing-library/user-event'
+import {render} from '@testing-library/react';
 import {AdultValidator} from '../src/AdultValidator';
 
 test('should show TO YOUNG message after entering value smaller than min', () => {
@@ -9,7 +10,7 @@ test('should show TO YOUNG message after entering value smaller than min', () =>
 
   // when
   const input = queryByLabelText(/age/i)
-  fireEvent.change(input, {target: {value: '2'}})
+  user.type(input, '2');
 
   // then
   expect(queryByRole('alert')).toHaveTextContent(/are you really so young/i)
@@ -21,7 +22,7 @@ test('should show TO OLD message after entering value greater than max', () => {
 
   // when
   const input = queryByLabelText(/age/i)
-  fireEvent.change(input, {target: {value: '200'}})
+  user.type(input, '200');
 
   // then
   expect(queryByRole('alert')).toHaveTextContent(/are you really so old/i)
@@ -33,7 +34,7 @@ test('should show ONLY FOR ADULT message after entering value smaller than 18', 
 
   // when
   const input = queryByLabelText(/age/i)
-  fireEvent.change(input, {target: {value: '15'}})
+  user.type(input, '15');
 
   // then
   expect(queryByRole('alert')).toHaveTextContent(/only for adult/i)
