26d49871aff19bcf3dfeaf67ee6457d31a63bc63
Dariusz Wojtowicz <dariusz.wojtowicz@allegro.pl>
2021-03-23 11:16:55 +0100

    Async Functions (HTTP requests) Solution

--- tests/async-functions-http.spec.tsx
+++ tests/async-functions-http.spec.tsx
@@ -3,15 +3,26 @@ import '@testing-library/jest-dom/extend-expect';
 import user from '@testing-library/user-event'
 import {render, waitFor} from '@testing-library/react';
 import {WeatherInfo} from '../src/WeatherInfo';
+import {fetchWeather} from '../src/utils/api';
+import {mocked} from 'ts-jest';
 
-test('should display weather after providing city name', () => {
+jest.mock('../src/utils/api');
+
+const fetchWeatherMock = mocked(fetchWeather);
+
+test('should display weather after providing city name', async () => {
   // given
+  fetchWeatherMock.mockResolvedValueOnce('sunny');
   const {queryByLabelText, queryByText} = render(<WeatherInfo />);
 
   // when
   const input = queryByLabelText(/city/i);
+  user.type(input, 'london');
 
   // then
-  // TODO fetchWeather was called with provided city name
-  // TODO weather for provided city name is displayed
+  await waitFor(() => {
+    expect(fetchWeatherMock).toHaveBeenCalledWith('london');
+    const weatherInfo = queryByText(/weather in london is sunny/i);
+    expect(weatherInfo).toBeInTheDocument();
+  });
 });
