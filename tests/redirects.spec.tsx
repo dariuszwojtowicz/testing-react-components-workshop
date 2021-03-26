5640172e6c598779b5d6fd9ebe5a15c99bac123d
Dariusz Wojtowicz <dariusz.wojtowicz@allegro.pl>
2021-03-26 08:23:45 +0100

    Redirects Solution

--- tests/redirects.spec.tsx
+++ tests/redirects.spec.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import {Router} from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
 import {createMemoryHistory} from 'history';
+import user from '@testing-library/user-event';
 import {render} from '@testing-library/react';
 import {RoutedApp} from '../src/RoutedApp';
 
 test('should RoutedApp component render contact and info and allow user to navigate', () => {
   // given
   const history = createMemoryHistory({initialEntries: ['/info']});
-  const {container} = render(
+  const {queryByRole, queryByText} = render(
     <Router history={history}>
       <RoutedApp />
     </Router>,
   );
-  // TODO check if Info header is displayed
-  expect(container.querySelector('h2')).toHaveTextContent(/info/i);
+  const initialHeading = queryByRole('heading');
+  expect(initialHeading).toHaveTextContent(/info/i);
 
   // when
-  // TODO navigate to Contact page
+  const contactMenuItem = queryByText(/contact/i);
+  user.click(contactMenuItem);
 
   // then
-  // TODO check if Contact header is displayed
+  const afterRedirectHeading = queryByRole('heading');
+  expect(afterRedirectHeading).toHaveTextContent(/contact/i);
 });
