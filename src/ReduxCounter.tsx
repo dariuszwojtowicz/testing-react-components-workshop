24ad47a9211641132a642460398ebd5d36ece4ef
Dariusz Wojtowicz <dariusz.wojtowicz@allegro.pl>
2021-03-31 07:18:48 +0200

    Use Effect

--- src/ReduxCounter.tsx
+++ src/ReduxCounter.tsx
@@ -1,15 +1,41 @@
 import * as React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import {useEffect, useRef, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from './redux/reducer';
+
+const usePrevious = (value) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 export const ReduxCounter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state: RootState) => state.counter);
+  const [info, setInfo] = useState('');
   const inc = () => dispatch({ type: 'INC' });
   const dec = () => dispatch({ type: 'DEC' });
+
+  const prevCounter: number = usePrevious(counter);
+
+  useEffect(() => {
+    if (typeof prevCounter !== 'undefined') {
+      if (counter > prevCounter) {
+        setInfo('Increment');
+      } else {
+        if (counter < prevCounter) {
+            setInfo('Decrement');
+          }
+      }
+    }
+  }, [counter, prevCounter]);
+
   return (
     <>
       <h1>Redux Counter</h1>
+      {info && <h2>Last operation: {info}</h2>}
       <button onClick={dec}>-</button>
       <label htmlFor="counter">Counter value:</label>
       <input id="counter" disabled value={counter} />
---
+++ tests/use-effect.spec.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import {Provider} from 'react-redux';
+import {ReduxCounter} from '../src/ReduxCounter';
+import {store} from '../src/redux/store';
+
+test('should display last operation info', () => {
+  // given
+  const {queryByText} = render(
+    <Provider store={store}>
+      <ReduxCounter />
+    </Provider>,
+  );
+
+  // when
+
+  // then
+  expect(queryByText(/last operation: increment/i)).toBeInTheDocument();
+
+  // when
+
+  // then
+  expect(queryByText(/last operation: decrement/i)).toBeInTheDocument();
+});
+
